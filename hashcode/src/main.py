a30ebd84cf672d35154bd3549a27aa5f13c3fc3b
JoaoBraveCoding <jooa.marcal12@gmail.com>
2020-02-25 07:50:21 +0100

    20m

--- hashcode/src/main.py
+++ hashcode/src/main.py
@@ -8,6 +8,9 @@ out_libs = []
 globalbooks = []
 books = []
 days_left = 0
+max_score = 0
+min_score = 100000000
+
 
 class lib:
     def __init__(self, number, sign_up, books, books_per_day):
@@ -17,46 +20,54 @@ class lib:
         self.books_per_day = books_per_day
         self.shiped_books = []
 
+
 class book:
     def __init__(self, book_id):
         self.book_id = book_id
         self.added = False
 
+
 def ss_in(file_name):
     # Parsing
     f = open(file_name, 'r')
 
-    global max_days, book_score, books, libs 
+    global max_days, book_score, books, libs, max_score, min_score
 
-    #First Line aka number of days
+    # First Line aka number of days
     first_line = f.readline().split(' ')
     max_days = int(first_line[2])
 
-    #Second line aka book scores
+    # Second line aka book scores
     second_line = f.readline().split(' ')
     i = 0
     for param in second_line:
-        book_score.append(int(param))
+        score = int(param)
+        book_score.append(score)
         books.append(book(i))
         i += 1
+        if max_score < score:
+            max_score = score
+        if min_score > score:
+            min_score = score
 
-
-    #Rest of the lines aka populate libs
-    #Reads two lines to create a lib
+    # Rest of the lines aka populate libs
+    # Reads two lines to create a lib
     i = 0
-    for _ in first_line[1]*2:
-        #Gets the first line with the sign_up and books_per_day
+    for _ in range(0, int(first_line[1])):
+        # Gets the first line with the sign_up and books_per_day
         lib_params = f.readline().split(' ')
 
-        #Gets the books
+        # Gets the books
         library_books = f.readline().split(' ')
         books_to_append = []
         for param in library_books:
             if param != '':
-                books_to_append.append( books[ int(param) ] )
-            
+                books_to_append.append(books[int(param)])
+
         organize_books(books_to_append)
-        libs.append(lib(i, int(lib_params[1]), books_to_append, int(lib_params[2])))
+        sign_up = int(lib_params[1])
+        books_per_day = int(lib_params[2])
+        libs.append(lib(i, sign_up, books_to_append, books_per_day))
         i += 1
 
 
@@ -69,7 +80,6 @@ def ss_out(file_name):
             tempS += str(j.book_id) + " "
         out += tempS + "\n"
 
-
     file_name_splited = file_name.split("/")
     f = open("hashcode/out/" + file_name_splited[3], 'w')
     f.writelines(out)
@@ -80,10 +90,17 @@ def ss_out(file_name):
 def value(elem):
     return book_score[elem.book_id]*-1
 
+
 def organize_books(book_array):
-    book_array.sort(key=value)
-    #TODO não sei se o parametro é alterado ou se o tenho de retornar
-    return book_array
+    tmp = book_array[:]
+    for i in range(len(book_array), 0):
+        if book_array[i].added:
+            tmp.pop(i)
+
+    tmp.sort(key=value)
+    book_array = tmp
+    return
+
 
 def sum_books_values(book_array):
     total = 0
@@ -91,76 +108,83 @@ def sum_books_values(book_array):
         total += book_score[book.book_id]
     return total
 
-def get_book(lib):
-    best_book = None
-    if len(lib.books) > 0:
-        while len(lib.books) >= 2:
-            best_book = lib.books[0]
-            lib.books = lib.books[1:]
-            if not best_book.added:
-                break
-        if len(lib.books) == 1 and (best_book == None) or (best_book.added and not lib.books[0].added):
-            best_book = lib.books[0]
-            lib.books = []
 
-        if best_book != None:
-            lib.shiped_books.append(best_book)
 
-    return None
+def get_books(lib, i):
+    max_shipped_book = (max_days - i) * lib.books_per_day
+    if len(lib.books) > max_shipped_book:
+        lib.shiped_books = lib.books[0:max_shipped_book]
+    else:
+        lib.shiped_books = lib.books
+
+    if lib.shiped_books == []:
+        print("JEy")
+
+    for j in lib.shiped_books:
+        j.added = True
+
 
 def get_lib_value(elem):
-    days_until_no_more_books = len(elem.books)/elem.books_per_day
+    # days_until_no_more_books = len(elem.books)/elem.books_per_day
 
-    days_it_still_can_supply = days_until_no_more_books - days_left
-    if days_it_still_can_supply < 0 :
-        days_it_still_can_supply = days_until_no_more_books
-    else:
-        days_it_still_can_supply = days_left
-    
+    # days_it_still_can_supply = days_until_no_more_books - days_left
+    # if days_it_still_can_supply < 0:
+    #     days_it_still_can_supply = days_until_no_more_books
+    # else:
+    #     days_it_still_can_supply = days_left
 
-    possible_remeinder_score = sum_books_values(elem.books) / days_left
-    return (possible_remeinder_score + days_it_still_can_supply)*-1
+    # possible_remeinder_score = sum_books_values(elem.books) / days_left
+
+    # return (possible_remeinder_score + days_it_still_can_supply)*-1
+    return elem.sign_up
+
 
 def sort_libs():
     global libs
     libs.sort(key=get_lib_value)
+
 
 def pick_new_lib(i):
     global out_libs, libs
 
     if len(libs) > 0:
         # Sort from once in a while
-        if i % 5 == 0:
-            sort_libs()
-        
+        sort_libs()
+
         # Retrice always the first lib
         picked_lib = libs[0]
         libs = libs[1:]
-        
+
+        time_when_ready = i + picked_lib.sign_up
+        if time_when_ready > max_days:
+            return None
+
         out_libs.append(picked_lib)
-        return picked_lib.sign_up
-    return 0
+        return picked_lib
+    return None
 
 
 def let_time_fly():
     global days_left
-    sigh_up = 0
-    for i in range(0, max_days):
+    i = 0
+    while i < max_days:
         days_left = max_days - 1 - i
-        if sigh_up == 0:
-            sigh_up = pick_new_lib(i)
-        sigh_up -= 1
-        
-        for lib in out_libs:
-            for _ in range(0, lib.books_per_day):
-                get_book(lib)
-    return 
+        latest_picked_lib = pick_new_lib(i)
+        if latest_picked_lib != None:
+            time_when_ready = i + latest_picked_lib.sign_up
+            get_books(latest_picked_lib, time_when_ready)
+            i += latest_picked_lib.sign_up
+        else:
+            return
+    return
+
 
 def main():
     # Input
     file_name = sys.argv[1]
     ss_in(file_name)
-    #Algorith
+
+    # Algorith
     let_time_fly()
 
     # Output
@@ -169,8 +193,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
